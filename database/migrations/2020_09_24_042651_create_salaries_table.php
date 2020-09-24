c0c7330afe6aa6aa614b85d05b3a068fb3ce226f
Muslehuddin Juned <MuslehuddinJuned@users.noreply.github.com>
2020-09-24 12:58:01 +0600

    add salary schema

---
+++ database/migrations/2020_09_24_042651_create_salaries_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salaries', function (Blueprint $table) {
+            $table->id();
+            $table->float('basic_pay')->nullable();
+            $table->float('medic_alw')->nullable();
+            $table->float('house_rent')->nullable();
+            $table->float('ta')->nullable();
+            $table->float('da')->nullable();
+            $table->string('other_field')->nullable();
+            $table->float('other_pay')->nullable();
+            $table->float('total_salary')->nullable();
+            $table->string('bank_name')->nullable();
+            $table->string('acc_no')->nullable();
+
+            $table->unsignedBigInteger('employee_id');
+            $table->timestamps();
+            $table->foreign('employee_id')->references('id')->on('employees')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salaries');
+    }
+}
