7e32daca6816b1b11466caab6b6d7270677323ac
Muslehuddin Juned <MuslehuddinJuned@users.noreply.github.com>
2020-10-20 12:34:07 +0600

    add salarysheet schema

---
+++ app/Http/Controllers/SalarysheetController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Salarysheet;
+use Illuminate\Http\Request;
+
+class SalarysheetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Salarysheet  $salarysheet
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Salarysheet $salarysheet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Salarysheet  $salarysheet
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Salarysheet $salarysheet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Salarysheet  $salarysheet
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Salarysheet $salarysheet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Salarysheet  $salarysheet
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Salarysheet $salarysheet)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_10_20_061321_create_salarysheets_table.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalarysheetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salarysheets', function (Blueprint $table) {
+            $table->id();
+            $table->string('year_month')->nullable();
+            $table->date('next_increment')->nullable();
+            $table->double('basic_monthly')->nullable();
+            $table->double('house_rent')->nullable();
+            $table->double('medic_allowance')->nullable();
+            $table->double('salary')->nullable();
+            $table->double('covert_rate')->nullable();
+            $table->double('ta')->nullable();
+            $table->double('da')->nullable();
+            $table->double('attendance_bonus')->nullable();
+            $table->double('production_bonus')->nullable();
+            $table->double('worked_friday_hour')->nullable();
+            $table->double('worked_friday_amount')->nullable();
+            $table->double('worked_holiday_hour')->nullable();
+            $table->double('worked_holiday_amount')->nullable();
+            $table->double('ot_rate')->nullable();
+            $table->double('ot_hour')->nullable();
+            $table->double('ot_amount')->nullable();
+            $table->double('fixed_allowance')->nullable();
+            $table->double('attendance_allowance')->nullable();
+            $table->double('present_days')->nullable();
+            $table->double('absent_days')->nullable();
+            $table->double('leave_days')->nullable();
+            $table->double('advance')->nullable();
+            $table->double('pf')->nullable();
+            $table->double('tax')->nullable();
+            $table->double('deducted')->nullable();
+            $table->double('not_for_join_days')->nullable();
+            $table->double('not_for_join_amount')->nullable();
+            $table->double('lay_off_days')->nullable();
+            $table->double('lay_off_amount')->nullable();
+            $table->double('suspense_days')->nullable();
+            $table->double('suspense_amount')->nullable();
+
+            $table->unsignedBigInteger('employee_id')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salarysheets');
+    }
+}
