eb8cbd83d6e53c94bc2963b588735770b1f4b15e
Muslehuddin Juned <MuslehuddinJuned@users.noreply.github.com>
2020-12-24 00:46:17 +0600

    make layout auto

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -11,8 +11,11 @@ $config = [
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 {{-- <html lang="{{ app()->getLocale() }}"> --}}
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  {{-- <meta charset="utf-8"> --}}
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  {{-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> --}}
   <meta name="keywords" content="SUSTipe, Human Resource Management, Payroll, Inventory Control, Inventory Management">
   <meta name="description" content="In order to cover all day-to-day HR tasks and personnel and payroll needs, you may take advantage of a Human Resource Management System (HRMS) ‒ specialized software that helps with HR activities.">
   <meta name="author" content="Muslehuddinjuned, Muslehuddin Juned, Musleh Uddin Juned, Muslehuddin, Juned">
