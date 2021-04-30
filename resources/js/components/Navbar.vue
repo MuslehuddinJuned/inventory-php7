cf31f59ae8e085c91f7d905ef7c46c9fd8ba88ac
Muslehuddin Juned <MuslehuddinJuned@users.noreply.github.com>
2021-05-01 00:06:32 +0600

    remove console

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -314,8 +314,6 @@ export default {
           mnth = ("0" + (date.getMonth() + 1)).slice(-2),
           day = '06'
 
-          console.log(date)
-
       var end = new Date([year, mnth, day].join("-"));
       var _second = 1000;
       var _minute = _second * 60;
